9603a034eaad4d9c999cf1cc68022d0577fcebae
mikereardon <michaelpjreardon@gmail.com>
2025-02-25 14:30:05 +0000

    Fixed locked_form partial to show actual quantity value instead of 1

--- app/models/rfq.rb
+++ app/models/rfq.rb
@@ -1,5 +1,5 @@
 class Rfq < ApplicationRecord
-  has_many :items, dependent: :destroy
+  has_many :items, inverse_of: :rfq, autosave: true, dependent: :destroy
   accepts_nested_attributes_for :items,
     reject_if: :all_blank, allow_destroy: true
   belongs_to :user
